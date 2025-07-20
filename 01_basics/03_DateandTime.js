649186eca7649fae4841011565603ef24f1bd4e2
jpareek95 <jpareek95@gmail.com>
2025-07-20 11:53:22 +0000

    dateandtime

---
+++ 01_basics/03_DateandTime.js
@@ -0,0 +1,23 @@
+let myDate= new Date()
+// console.log(myDate)
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+let myDateTIme= new Date(2025,0,19,6,20,29)
+// console.log(myDateTIme.toString())
+// console.log(myDateTIme.toLocaleDateString())
+// console.log(myDateTIme.toDateString())
+
+let mydateformat= new Date ("19-01-2025") //Need to check
+// console.log(mydateformat.toLocaleString())
+
+let getmonth = new Date()
+console.log(getmonth.getDay())
+console.log(getmonth.getHours())
+console.log(getmonth.getMonth())
+
+let Date1=new Date()
+console.log("Date is:", Date1)
